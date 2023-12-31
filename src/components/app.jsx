3d7e55bb95d6da99f67d6e0b420e3bdc2d2101cd
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2024-01-01 08:14:14 +1300

    When clicking different cards, it shuffles. If they picked the same card it stops.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,11 @@
 import ImageGenerate from "./image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import shuffle from "../functions/shuffle";
 
 export default function AppGenerate () {
     const [card, setCard] = useState([]);
     const [randomize, setRandomize] = useState(true);
+    const [images, setImages] = useState([]);
 
     const url = [
         "https://pokeapi.co/api/v2/pokemon/psyduck",
@@ -14,17 +15,28 @@ export default function AppGenerate () {
         "https://pokeapi.co/api/v2/pokemon/ditto"
     ];
 
-    const array = [
-        <ImageGenerate  url={url[0]} getter={{card, randomize}} setter={{setCard, setRandomize}} />,
-        <ImageGenerate  url={url[1]} getter={{card, randomize}} setter={{setCard, setRandomize}} />,
-    ];
+    useEffect(() => {
+        const array = [
+            <ImageGenerate  url={url[0]} getter={{card, randomize, images}} setter={{setCard, setRandomize, setImages}} id={1} />,
+            <ImageGenerate  url={url[1]} getter={{card, randomize, images}} setter={{setCard, setRandomize, setImages}} id={2} />,
+            <ImageGenerate  url={url[2]} getter={{card, randomize, images}} setter={{setCard, setRandomize, setImages}} id={3} />,
+            <ImageGenerate  url={url[3]} getter={{card, randomize, images}} setter={{setCard, setRandomize, setImages}} id={4} />,
+            <ImageGenerate  url={url[4]} getter={{card, randomize, images}} setter={{setCard, setRandomize, setImages}} id={5} />,
+        ];
+
+        setImages(shuffle(array));
+    }, [card])
 
     if (randomize == true) {
         return (
-            <div style={{display: 'flex', flexDirection: 'row'}}>
-                {shuffle(array)}
+            <div style={{display: 'flex', flexDirection: 'row'}} > 
+                {images}
             </div>
         );
     }
+
+    return (
+        <div>Retry?</div>
+    );
 }
 
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,23 +1,24 @@
 import requestObject from "../functions/request_object";
 import shuffle from "../functions/shuffle";
 
-export default function ImageGenerate ({url, getter={}, setter={}}) {
+export default function ImageGenerate ({url, getter={}, setter={}, id}) {
     const {object, pending} = requestObject(url);
-    if (pending == true) {return <div>Loading...</div>}
+    if (pending == true) {return <div id={id}>Loading...</div>}
     return (
         <div onClick={() => {
-            const {card, randomize} = getter;
-            const {setCard, setRandomize} = setter;
+            const {card, randomize, images} = getter;
+            const {setCard, setRandomize, setImages} = setter;
 
             for (let i = 0; i < card.length; i++) {
                 if (card[i] == object.name) {
-                    //setRandomize(false);
+                    setRandomize(false);
                     return console.log(card, randomize);
                 }
             }
             
             setCard(previous => [...previous, object.name]);
-        }}>
+            setImages(shuffle(images));
+        }} id={id}>
             <img src={object.sprite} style={{background: 'white'}}/>
             <div>{object.name}</div>
         </div>
