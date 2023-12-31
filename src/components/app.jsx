4831cb22ea2f0654594e98a006e7ad2d26ef6736
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-12-31 23:34:27 +1300

    Refactoring

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,31 @@
 import ImageGenerate from "./image";
+import { useState, useEffect } from "react";
+import shuffle from "../functions/shuffle";
 
 export default function AppGenerate () {
-    return (
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-            <ImageGenerate  url={"https://pokeapi.co/api/v2/pokemon/psyduck"}/>
-            <ImageGenerate url={"https://pokeapi.co/api/v2/pokemon/sprigatito"}/>
-            <ImageGenerate url={"https://pokeapi.co/api/v2/pokemon/magikarp"}/>
-            <ImageGenerate url={"https://pokeapi.co/api/v2/pokemon/meowth"}/>
-            <ImageGenerate url={"https://pokeapi.co/api/v2/pokemon/ditto"}/>
-        </div>
-    );
+    const [card, setCard] = useState([]);
+    const [randomize, setRandomize] = useState(true);
+    const [images, setImages] = useState([]);
+
+    const url = [
+        "https://pokeapi.co/api/v2/pokemon/psyduck",
+        "https://pokeapi.co/api/v2/pokemon/sprigatito",
+        "https://pokeapi.co/api/v2/pokemon/magikarp",
+        "https://pokeapi.co/api/v2/pokemon/meowth",
+        "https://pokeapi.co/api/v2/pokemon/ditto"
+    ];
+
+    const array = [
+        <ImageGenerate  url={url[0]} getter={{card, randomize, images}} setter={{setCard, setRandomize, setImages}} />,
+        <ImageGenerate  url={url[1]} getter={{card, randomize, images}} setter={{setCard, setRandomize, setImages}} />,
+    ];
+
+    if (randomize == true) {
+        return (
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                {array}
+            </div>
+        );
+    }
 }
 
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,10 +1,23 @@
-import requestObject from "./request_object";
+import requestObject from "../functions/request_object";
+import shuffle from "../functions/shuffle";
 
-export default function ImageGenerate ({url}) {
+export default function ImageGenerate ({url, getter={}, setter={}}) {
     const {object, pending} = requestObject(url);
     if (pending == true) {return <div>Loading...</div>}
     return (
-        <div onClick={() => {console.log(object.name)}}>
+        <div onClick={() => {
+            const {card, randomize} = getter;
+            const {setCard, setRandomize} = setter;
+
+            for (let i = 0; i < card.length; i++) {
+                if (card[i] == object.name) {
+                    //setRandomize(false);
+                    return console.log(card, randomize);
+                }
+            }
+           
+            setCard(previous => [...previous, object.name]);
+        }}>
             <img src={object.sprite} style={{background: 'white'}}/>
             <div>{object.name}</div>
         </div>
